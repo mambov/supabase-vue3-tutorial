2adc37de9a1316119c3d4ed07a79318b0a291ae7
Diego V <diego@codewith.mobi>
2024-07-10 11:05:15 -0500

    feat: add code from official supabase

--- supabase-vue-3/src/components/Account.vue
+++ supabase-vue-3/src/components/Account.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { supabase } from '../supabase'
 import { onMounted, ref, toRefs } from 'vue'
-import Avatar from './Avatar.vue'
+import Avatar from './Avatar.vue';
 
 const props = defineProps(['session'])
 const { session } = toRefs(props)
@@ -12,98 +12,95 @@ const website = ref('')
 const avatar_url = ref('')
 
 onMounted(() => {
-  getProfile()
+    getProfile()
 })
 
 async function getProfile() {
-  try {
-    loading.value = true
-    const { user } = session.value
+    try {
+        loading.value = true
+        const { user } = session.value
 
-    const { data, error, status } = await supabase
-      .from('profiles')
-      .select(`username, website, avatar_url`)
-      .eq('id', user.id)
-      .single()
+        let { data, error, status } = await supabase
+            .from('profiles')
+            .select(`username, website, avatar_url`)
+            .eq('id', user.id)
+            .single()
 
-    if (error && status !== 406) throw error
+        if (error && status !== 406) throw error
 
-    if (data) {
-      username.value = data.username
-      website.value = data.website
-      avatar_url.value = data.avatar_url
+        if (data) {
+            username.value = data.username
+            website.value = data.website
+            avatar_url.value = data.avatar_url
+        }
+    } catch (error) {
+        alert(error.message)
+    } finally {
+        loading.value = false
     }
-  } catch (error) {
-    alert(error.message)
-  } finally {
-    loading.value = false
-  }
 }
 
 async function updateProfile() {
-  try {
-    loading.value = true
-    const { user } = session.value
+    try {
+        loading.value = true
+        const { user } = session.value
 
-    const updates = {
-      id: user.id,
-      username: username.value,
-      website: website.value,
-      avatar_url: avatar_url.value,
-      updated_at: new Date(),
+        const updates = {
+            id: user.id,
+            username: username.value,
+            website: website.value,
+            avatar_url: avatar_url.value,
+            updated_at: new Date(),
+        }
+
+        let { error } = await supabase.from('profiles').upsert(updates)
+
+        if (error) throw error
+    } catch (error) {
+        alert(error.message)
+    } finally {
+        loading.value = false
     }
-
-    const { error } = await supabase.from('profiles').upsert(updates)
-
-    if (error) throw error
-  } catch (error) {
-    alert(error.message)
-  } finally {
-    loading.value = false
-  }
 }
 
 async function signOut() {
-  try {
-    loading.value = true
-    const { error } = await supabase.auth.signOut()
-    if (error) throw error
-  } catch (error) {
-    alert(error.message)
-  } finally {
-    loading.value = false
-  }
+    try {
+        loading.value = true
+        let { error } = await supabase.auth.signOut()
+        if (error) throw error
+    } catch (error) {
+        alert(error.message)
+    } finally {
+        loading.value = false
+    }
 }
 </script>
 
 <template>
-  <form class="form-widget" @submit.prevent="updateProfile">
-      <!-- Add to body -->
-    <Avatar v-model:path="avatar_url" @upload="updateProfile" size="10" />
-    <div>
-      <label for="email">Email</label>
-      <input id="email" type="text" :value="session.user.email" disabled />
-    </div>
-    <div>
-      <label for="username">Name</label>
-      <input id="username" type="text" v-model="username" />
-    </div>
-    <div>
-      <label for="website">Website</label>
-      <input id="website" type="url" v-model="website" />
-    </div>
+    <form class="form-widget" @submit.prevent="updateProfile">
+        <Avatar v-model:path="avatar_url" @upload="updateProfile" size="10" />
+        <div>
+            <label for="email">Email</label>
+            <input id="email" type="text" :value="session.user.email" disabled />
+        </div>
+        <div>
+            <label for="username">Name</label>
+            <input id="username" type="text" v-model="username" />
+        </div>
+        <div>
+            <label for="website">Website</label>
+            <input id="website" type="url" v-model="website" />
+        </div>
 
-    <div>
-      <input
-        type="submit"
-        class="button primary block"
-        :value="loading ? 'Loading ...' : 'Update'"
-        :disabled="loading"
-      />
-    </div>
+        <div>
+            <input type="submit" class="button primary block" :value="loading ? 'Loading ...' : 'Update'"
+                :disabled="loading" />
+        </div>
 
-    <div>
-      <button class="button block" @click="signOut" :disabled="loading">Sign Out</button>
-    </div>
-  </form>
+        <div>
+            <button class="button block" @click="signOut" :disabled="loading">
+                Sign Out
+            </button>
+        </div>
+    </form>
 </template>
